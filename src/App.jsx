1ad638b867ba5587169ccb396e6bd670f65a4e4a
bobohdana <stetsenkobohdana@gmail.com>
2024-12-12 13:23:54 +0200

    create routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
+import { Routes, Route } from 'react-router';
+
+import Home from './pages/Home';
+import Service from './pages/Service';
+import About from './pages/About';
+import Info from './pages/Info';
 
 const App = () => {
   return (
-    <div>HELLO</div>
+    <Routes>
+      <Route index element={<Home />} />
+      <Route path="services">
+        <Route path="tech-consulting" element={<Service />} />
+        <Route path="software-development" element={<Service />} />
+      </Route>
+      <Route path="about" element={<About />} />
+
+      <Route path="privacy-policy" element={<Info />} />
+      <Route path="terms-and-conditions" element={<Info />} />
+      <Route path="legal-notice" element={<Info />} />
+    </Routes>
   )
 }
 
