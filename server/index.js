8e1d8d4622c7f8fa1e1e890948ecf99d07731441
bobohdana <stetsenkobohdana@gmail.com>
2025-01-03 23:09:37 +0200

    add cors

--- server/index.js
+++ server/index.js
@@ -14,6 +14,9 @@ require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
+app.use(cors({
+  origin: 'https://abnex-lyart.vercel.app/'
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
